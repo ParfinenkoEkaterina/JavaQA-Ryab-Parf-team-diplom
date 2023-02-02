714cf0c157f6b72b2031e48c6096cbadfaab0ce4
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-02-02 12:00:02 +0300

    Добавлен тест подсчёта часов для каждой игры

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,23 +1,61 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
 
+    private GameStore store = null;
+    Game game1 = null;
+    Game game2 = null;
+    Game game3 = null;
+    Game game4 = null;
+    Game game5 = null;
+    Game game6 = null;
+
+
+    @BeforeEach
+    public void beforeEach() {
+        store = new GameStore();
+        game1 = store.publishGame("Mario", "Arcade");
+        game2 = store.publishGame( "Invaders", "Arcade");
+        game3 = store.publishGame("Top Gun", "Simulator");
+        game4 = store.publishGame("F1", "Simulator");
+        game5 = store.publishGame("KungFu", "Fighting");
+        game6 = store.publishGame("Mortal Combat", "Fighting");
+
+    }
+
     @Test
     public void shouldSumGenreIfOneGame() {
-        GameStore store = new GameStore();
-        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
         Player player = new Player("Petya");
-        player.installGame(game);
-        player.play(game, 3);
+        player.installGame(game1);
+        player.play(game1, 3);
 
         int expected = 3;
-        int actual = player.sumGenre(game.getGenre());
+        int actual = player.sumGenre(game1.getGenre());
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
+    @Test
+    public void shouldSumGenreIfTwoGames() {
+
+        Player player = new Player( "Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.play(game1 , 5);
+        player.play(game2 , 4);
+
+        int expected = 9;
+        int actual = player.sumGenre(game1.getGenre());
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void shouldSumGenreIf
+
+
 }
