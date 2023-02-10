191be6bfb757a0a142bacb1de9450bf5be30d3ce
pricheska <den-ryab2019@yandex.ru>
2023-02-11 00:41:24 +0400

    Add tests on class Gamestore

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
@@ -13,6 +15,29 @@ public class GameStoreTest {
 
         assertTrue(store.containsGame(game));
     }
+    @Test
+    public void ShouldShowIfContainGame() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("katana Zero", "Slasher");
 
+        boolean expected = true;
+        boolean actual = store.containsGame(game);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddPlayTime() {
+        GameStore store = new GameStore();
+
+        String playerName = "Dima";
+        int hours = 1;
+
+        store.addPlayTime(playerName, hours);
+        store.addPlayTime(playerName, hours);
+
+        Assertions.assertEquals(hours * 2, store.getSumPlayedTime());
+
+    }
     // другие ваши тесты
 }
